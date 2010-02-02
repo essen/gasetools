b113796c2de5c20ebfccd307da5cf00a4f48df45
Loïc Hoguin <essen@dev-extend.eu>
2010-02-02 02:45:27 +0100

    Initial work on the fpb extractor.

---
+++ fpb/main.c
@@ -0,0 +1,85 @@
+/*
+	gasetools: a set of tools to manipulate SEGA games file formats
+	Copyright (C) 2010  Loic Hoguin
+
+	This file is part of gasetools.
+
+	gasetools is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	gasetools is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with gasetools.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "../nbl/nbl.h"
+
+/**
+ * We're just going through every NBL_CHUNK_PADDING_SIZE and extract all the nbl files we find.
+ */
+
+int main(int argc, char** argv)
+{
+	FILE* pFile;
+	FILE* pOut;
+	char* pstrBuffer;
+	char pstrFilename[16];
+	int iCurrentPos = 0;
+	int i, iRead, iTotal;
+	int aFiles[5000];
+	unsigned int iTmp;
+
+	if (2 != argc) {
+		fprintf(stderr, "Usage: %s file.fpb\n", argv[0]);
+		return 2;
+	}
+
+	pFile = fopen(argv[1], "rb");
+	if (pFile == NULL)
+		return -1;
+
+	iTotal = 0;
+	while (1) {
+		fseek(pFile, iCurrentPos, SEEK_SET);
+		iRead = fread(&iTmp, sizeof(unsigned int), 1, pFile);
+
+		if (iRead <= 0)
+			break;
+
+		if (iTmp == NBL_ID_NMLL || iTmp == NBL_ID_NMLB) {
+			aFiles[iTotal] = iCurrentPos;
+			iTotal++;
+		}
+
+		iCurrentPos += NBL_CHUNK_PADDING_SIZE;
+	}
+
+	fseek(pFile, 0, SEEK_END);
+	aFiles[iTotal] = ftell(pFile);
+
+	for (i = 0; i < iTotal; i++) {
+		fseek(pFile, aFiles[i], SEEK_SET);
+		pstrBuffer = malloc(aFiles[i + 1] - aFiles[i]);
+		iRead = fread(pstrBuffer, 1, aFiles[i + 1] - aFiles[i], pFile);
+		if (iRead > 0) {
+			sprintf(pstrFilename, "%d.nbl", i);
+			pOut = fopen(pstrFilename, "wb");
+			fwrite(pstrBuffer, 1, aFiles[i + 1] - aFiles[i], pOut);
+			fclose(pOut);
+		}
+
+		free(pstrBuffer);
+	}
+
+	fclose(pFile);
+
+	return 0;
+}
