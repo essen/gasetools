02a93705f8eff336c592fd4a449276cbfacdddfb
Loïc Hoguin <essen@dev-extend.eu>
2010-09-21 19:01:05 +0200

    proxy: Log game data in a single log file using nyu's log format for compatibility.

--- proxy/psu_proxy.erl
+++ proxy/psu_proxy.erl
@@ -7,11 +7,13 @@
 -define(LOGIN_HOST, "loginpc.ipsu.segaonline.jp").
 -define(LOGIN_PORT, 12030).
 
--define(LISTEN_OPTIONS, [binary, {active, false}, {certfile, "servercert.pem"}, {keyfile, "serverkey.pem"}, {password, "alpha"}]).
--define(CONNECT_OPTIONS, [binary, {active, false}, {certfile, "clientcert.pem"}, {keyfile, "clientkey.pem"}, {password, "alpha"}]).
+-define(LISTEN_OPTIONS, [binary, {active, false}, {reuseaddr, true}, {ssl_imp, new}, {certfile, "servercert.pem"}, {keyfile, "serverkey.pem"}, {password, "alpha"}]).
+-define(CONNECT_OPTIONS, [binary, {active, false}, {reuseaddr, true}, {ssl_imp, new}, {certfile, "clientcert.pem"}, {keyfile, "clientkey.pem"}, {password, "alpha"}]).
 
 start() ->
 	% ssl
+	application:start(crypto),
+	application:start(public_key),
 	application:start(ssl),
 	ssl:seed("Suits up!"), % TODO: should be random
 	% start
@@ -29,21 +31,11 @@ login_accept(LSocket) ->
 	spawn(fun() -> login_proxy(SSocket, CSocket, "login-server", 1) end),
 	login_accept(LSocket).
 
-test(CSocket) ->
-	case ssl:recv(CSocket, 0, 50) of
-		{ok, Packet} ->
-			file:write_file("test.bin", Packet);
-		{error, closed} ->
-			io:format("socket closed~n");
-		{error, timeout} ->
-			test(CSocket)
-	end.
-
 login_spoof_game(SocketSend, Packet) ->
 	io:format("spoofing game server IP~n"),
 	<<Before:352/bits, GameIP:32/bits, GamePort:16/little-unsigned-integer, After/bits>> = Packet,
 	% TODO: game IP is still hardcoded
-	SpoofedPacket = <<Before/bits, 192, 168, 1, 15, GamePort:16/little-unsigned-integer, After/bits>>,
+	SpoofedPacket = <<Before/bits, 192, 168, 1, 42, GamePort:16/little-unsigned-integer, After/bits>>,
 	spawn(fun() -> game_listen(GameIP, GamePort) end),
 	ssl:send(SocketSend, SpoofedPacket).
 
@@ -51,12 +43,12 @@ login_proxy(SocketRecv, SocketSend, Name, Number) ->
 	case ssl:recv(SocketRecv, 0, 50) of
 		{ok, Packet} ->
 			<<_:32, Command:24/unsigned-integer, _/bits>> = Packet,
+			io:format("handling login packet: ~s-~.10B~n", [Name, Number]),
+			file:write_file(lists:concat([Name, "-packet-", Number, ".bin"]), Packet),
 			case Command of
 				16#021603 ->
 					login_spoof_game(SocketSend, Packet);
 				_ ->
-					io:format("handling login packet: ~s-~.10B~n", [Name, Number]),
-					file:write_file(lists:concat([Name, "-packet-", Number, ".bin"]), Packet),
 					ssl:send(SocketSend, Packet),
 					login_proxy(SocketRecv, SocketSend, Name, Number + 1)
 			end;
@@ -77,15 +69,15 @@ game_accept(LSocket, GameIP, GamePort) ->
 	<<A:8/little-unsigned-integer, B:8/little-unsigned-integer, C:8/little-unsigned-integer, D:8/little-unsigned-integer>> = GameIP,
 	{ok, SSocket} = ssl:connect({A, B, C, D}, GamePort, ?CONNECT_OPTIONS),
 	io:format("game server ~.10B.~.10B.~.10B.~.10B:~.10B connected!~n", [A, B, C, D, GamePort]),
-	spawn(fun() -> game_proxy(CSocket, SSocket, "game-client", 1) end),
-	spawn(fun() -> game_proxy(SSocket, CSocket, "game-server", 1) end),
+	spawn(fun() -> game_proxy(CSocket, SSocket, << 16#43, 16#4c, 16#4e, 16#54 >>, 1) end),
+	spawn(fun() -> game_proxy(SSocket, CSocket, << 16#53, 16#45, 16#52, 16#56 >>, 1) end),
 	game_accept(LSocket, GameIP, GamePort).
 
 game_proxy(SocketRecv, SocketSend, Name, Number) ->
 	case ssl:recv(SocketRecv, 0, 50) of
 		{ok, Packet} ->
 			io:format("handling game packet: ~s-~.10B~n", [Name, Number]),
-			file:write_file(lists:concat([Name, "-packet-", Number, ".bin"]), Packet),
+			file:write_file("log.bin", << Name/binary, Number:32/little-unsigned-integer, Packet/binary >>, [append]),
 			ssl:send(SocketSend, Packet),
 			game_proxy(SocketRecv, SocketSend, Name, Number + 1);
 		{error, closed} ->
